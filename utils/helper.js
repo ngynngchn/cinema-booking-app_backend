38c5eae92c31f8769fb0549780e691cf1bd70186
ngynngchn <ngynngchn@gmail.com>
2023-07-03 13:33:32 +0200

    identify screenings by files inside screening directory

--- utils/helper.js
+++ utils/helper.js
@@ -3,8 +3,10 @@ import fs from "fs";
 export function read(path) {
 	return new Promise((resolve, reject) => {
 		fs.readFile(`./screenings/${path}.json`, (err, data) => {
-			if (err) reject(err);
-			else {
+			if (err) {
+				reject(err);
+				console.log("Something went wront while reading file");
+			} else {
 				console.log("Reading screening for movie id", path);
 				resolve(JSON.parse(data.toString()));
 			}
@@ -74,5 +76,18 @@ export function updateJSON(data, path) {
 					.catch((err) => reject(err));
 			})
 			.catch((err) => reject(err));
+	});
+}
+
+export function readDirectory(path) {
+	return new Promise((resolve, reject) => {
+		fs.readdir(path, (err, files) => {
+			if (err) {
+				reject(err);
+			} else {
+				console.log("Reading directory at", path);
+				resolve(files);
+			}
+		});
 	});
 }
